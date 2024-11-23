e60e393789766abdfae6257d2ea4e5409e78e9aa
SatyaKumar <satyakumarchaudhary603@gmail.com>
2024-11-23 18:21:21 +0530

    first header upper part is done

--- autosquare/src/Components/Header.js
+++ autosquare/src/Components/Header.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faUserAlt } from '@fortawesome/free-solid-svg-icons';  
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import { faHouse } from '@fortawesome/free-solid-svg-icons';
  import './header.css';
 const header=()=>{
     return(
@@ -9,11 +12,12 @@ const header=()=>{
             <h1 className='h1'>AutoSquare</h1>
             <h2 className='h2'>(888)748-0882|Mon-Fri:8AM-7PM EST
             </h2>
-            <ul>
-                    <li><FontAwesomeIcon icon={faSearch} /></li>
-                    <li><FontAwesomeIcon icon={faSearch} /></li>
-                    <li><FontAwesomeIcon icon={faSearch} /></li>
-                    <li><FontAwesomeIcon icon={faSearch} /></li>
+            <ul className='search'>
+                    <li className='gap'><FontAwesomeIcon icon={faSearch} /></li>
+                    <li className='gap'><FontAwesomeIcon icon={faCartShopping} /></li>
+                    <li className='gap'><FontAwesomeIcon icon={faHouse} /></li>
+                    <li className='gap'><FontAwesomeIcon icon={faUserAlt} /></li>
+                    
             </ul>
 
             </div>
